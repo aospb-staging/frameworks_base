b722a941238f83312b1f1768b047689be10da0fe
LibXZR <i@xzr.moe>
2022-03-18 10:12:37 +0800

    base: Add support for window ignore secure [1/2]
    Feature: yt/KSCOPE-23
    Change-Id: Ia9447ca17f6b24152b64526c25710a77e1388ce6
    Signed-off-by: LibXZR <i@xzr.moe>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -19786,6 +19786,13 @@ public final class Settings {
         public static final String SCREEN_CAPTURE_OVERRIDE = "screen_capture_override";
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * Setting indicating whether Low Power Standby is enabled, if supported.
          *
          * Values are:
