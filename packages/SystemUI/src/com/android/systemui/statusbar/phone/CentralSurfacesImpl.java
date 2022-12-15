93e3d2e872644a95ff5398bb0aaae54df80dcc74
Andrew Fluck <andrew@aospa.co>
2022-12-15 16:08:56 +0100

    SystemUI: Animate auto brightness toggle
    Also enforce restriction for toggle and fix the visibility of the
    toggle if the device does not support auto brightness
    
    Author: Andrew Fluck <andrew@aospa.co>
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>
    Change-Id: Ia80713220e47948a3cdf244f7bf8f69ce4421c08

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1320,7 +1320,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                             .withDefault(this::createDefaultQSFragment)
                             .build());
             mBrightnessMirrorController = new BrightnessMirrorController(
-                    mContext,
                     getNotificationShadeWindowView(),
                     mShadeSurface,
                     mNotificationShadeDepthControllerLazy.get(),
