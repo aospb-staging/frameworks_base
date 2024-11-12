a8e62ca71d48eccfd97d94ae13fda603a52c2ab3
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-12 17:15:54 +0530

    SystemUI: Bring back good ol' circle battery style
    Co-authored-by: althafvly <althafvly@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6387,6 +6387,26 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery as a circle
+         * 2: Display the battery as plain text
+         * default: 0
+         * 
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+        /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * 
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_battery_percent";
+
+        /**
          * Whether the torch launch gesture when the screen is off should be enabled.
          * @hide
          */
