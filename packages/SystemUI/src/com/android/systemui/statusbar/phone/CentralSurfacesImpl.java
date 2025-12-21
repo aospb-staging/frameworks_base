bd922b49f0da7a07d6947b39f4e59375c76eca2f
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-12-21 22:08:37 +0000

    Revert "SystemUIBoostframework: implement adpf boosting"
    This reverts commit df33f2d3b4c8d7ccdcf8190163d2e36c8e1f8eb9.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -59,7 +59,6 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.PerformanceHintManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -861,9 +860,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         if (PredictiveBackSysUiFlag.isEnabled()) {
             mContext.getApplicationInfo().setEnableOnBackInvokedCallback(true);
         }
-        PerformanceHintManager performanceHintManager =
-          (PerformanceHintManager) context.getSystemService(Context.PERFORMANCE_HINT_SERVICE);
-        com.android.systemui.util.SystemUIBoostFramework.getInstance().createAdpfSession(performanceHintManager);
     }
 
     private void initBubbles(Bubbles bubbles) {
