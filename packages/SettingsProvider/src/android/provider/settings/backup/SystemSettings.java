yaap/frameworks_base_new

base: Allow suppressing notifications when the screen is on [1/2]
Squashed:

    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Date: Sun, 30 Jun 2024 18:35:59 +0900
    Subject: fixup! Allow to suppress notifications sound/vibration if screen is ON [1/2]

    * Also apply it to NotificationAttentionHelper

    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Date: Tue, 23 Jul 2024 01:12:58 +0530
    Subject: fixup! Allow to suppress notifications
     sound/vibration if screen is ON [1/2]

    * We forgot to load settings on boot.

    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

idoybh (YAAP): Why u no backup? Also improve comments and wording

Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -189,7 +189,8 @@ public class SystemSettings {
                 Settings.System.NAVBAR_LONG_PRESS_GESTURE,
                 Settings.System.RAMPING_RINGER_DURATION,
                 Settings.System.RAMPING_RINGER_START_VOLUME,
-                Settings.System.RAMPING_RINGER_NO_SILENCE
+                Settings.System.RAMPING_RINGER_NO_SILENCE,
+                Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

aospb-staging/frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -120,7 +120,8 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
-                Settings.System.PREFERRED_REGION
+                Settings.System.PREFERRED_REGION,
+                Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
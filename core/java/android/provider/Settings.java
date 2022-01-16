dbbf20f736db68ad00ebc50cda459f66777fad26
ezio84 <brabus84@gmail.com>
2022-01-16 13:20:36 +0400

    base: Allow suppressing notifications when the screen is on [1/2]
    Squashed:
    
        Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
        Date: Sun, 30 Jun 2024 18:35:59 +0900
        Subject: fixup! Allow to suppress notifications sound/vibration if screen is ON [1/2]
    
        * Also apply it to NotificationAttentionHelper
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
        Date: Tue, 23 Jul 2024 01:12:58 +0530
        Subject: fixup! Allow to suppress notifications
         sound/vibration if screen is ON [1/2]
    
        * We forgot to load settings on boot.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    idoybh (YAAP): Why u no backup? Also improve comments and wording
    
    Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6510,6 +6510,12 @@ public final class Settings {
                 "high_touch_sensitivity_enable";
 
         /**
+         * Whether to play notification sound and vibrations when the screen is on
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
